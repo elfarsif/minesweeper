d9c8c41dc045dea1bfd7a01204ab354218b223cf
elfarsif <f.n.elfarsi@gmail.com>
2024-07-28 11:09:19 -0400

    refactor : extracted rendering to seperate class

--- src/main/java/org/frank/Game.java
+++ src/main/java/org/frank/Game.java
@@ -1,40 +1,18 @@
 package org.frank;
 
+import org.frank.Renderer;
+
 public class Game {
     int[][] board = new int[8][8];
 
     public void start() {
-        renderBoard();
+        Renderer.render(board);
     }
 
     public void click(int row, int col) {
         board[row][col] = State.EMPTY.value;
-        renderBoard();
+        Renderer.render(board);
     }
 
-    private void renderBoard() {
-        for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 8; col++) {
-                renderCell(row, col);
-            }
-            renderLastCell(row);
-        }
-    }
-
-    private void renderCell(int row, int col) {
-        if (board[row][col] == State.EMPTY.value) {
-            System.out.print("|   ");
-        } else {
-            System.out.print("|___");
-        }
-    }
-
-    private void renderLastCell(int row) {
-        if (row == board.length - 1) {
-            System.out.print("|");
-        } else {
-            System.out.print("|\n");
-        }
-    }
 }
 
---
+++ src/main/java/org/frank/Renderer.java
@@ -0,0 +1,29 @@
+package org.frank;
+
+public class Renderer {
+
+    public static void render(int[][] board) {
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                renderCell(board, row, col);
+            }
+            renderLastCell(board, row);
+        }
+    }
+
+    private static void renderCell(int[][] board, int row, int col) {
+        if (board[row][col] == State.EMPTY.value) {
+            System.out.print("|   ");
+        } else {
+            System.out.print("|___");
+        }
+    }
+
+    private static void renderLastCell(int[][] board, int row) {
+        if (row == board.length - 1) {
+            System.out.print("|");
+        } else {
+            System.out.print("|\n");
+        }
+    }
+}
