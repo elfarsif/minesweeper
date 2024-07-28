2e0b9c3e9e6429fb98c391114a59cff074008476
elfarsif <f.n.elfarsi@gmail.com>
2024-07-27 20:58:13 -0400

    feat : print the game board

--- src/test/java/org/frank/MineSweeperTest.java
+++ src/test/java/org/frank/MineSweeperTest.java
@@ -1,9 +1,41 @@
 package org.frank;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-class MineSweeperTest {
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
+class GameTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void acceptanceTest_whenStartsTheApplication_itShouldShowGameBoard() {
+        Game game = new Game();
+        game.start();
+        assertThat(outContent.toString())
+                .isEqualTo("|___|___|___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|");
+    }
 
 }
