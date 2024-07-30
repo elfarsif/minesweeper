3fea9d9a1ebd78be5062dcd92b8e28ece8cf1007
elfarsif <f.n.elfarsi@gmail.com>
2024-07-29 23:33:50 -0400

    test-red : acceptance test to see if placing a mine and clicking on a nearby spot renders correct number

---
+++ src/test/java/org/frank/acceptanceTest/MarkerTests.java
@@ -0,0 +1,40 @@
+package org.frank.acceptanceTest;
+
+import org.frank.Game;
+import org.frank.util.StreamTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MarkerTests extends StreamTest {
+    private Game game;
+
+    @BeforeEach
+    public void setUpGame() {
+        game = new Game();
+    }
+
+    @Test
+    void whenISetAMineAtLocation_thenClickMine_itShouldRevealMine() {
+        game.placeMine(0, 0);
+        game.click(0, 1);
+        assertThat(outContent.toString())
+                .isEqualTo("|___| 1 |___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|\n" +
+                        "|___|___|___|___|___|___|___|___|");
+    }
+
+    void provideInput(String data) {
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+}
