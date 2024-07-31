76b0b3ec9f56db71efb67642e736b9375cc91ca0
elfarsif <f.n.elfarsi@gmail.com>
2024-07-30 21:02:28 -0400

    red : acceptance test added test to have prompt for user input after grid on start

--- src/test/java/org/frank/TestSuite.java
+++ src/test/java/org/frank/TestSuite.java
@@ -1,10 +1,12 @@
 package org.frank;
 
-import org.junit.platform.suite.api.IncludePackages;
+import org.frank.acceptanceTest.UserStoryFourteen;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectPackages({"org.frank.acceptanceTest", "org.frank"})
+@SelectClasses({UserStoryFourteen.class})
 public class TestSuite {
 }
